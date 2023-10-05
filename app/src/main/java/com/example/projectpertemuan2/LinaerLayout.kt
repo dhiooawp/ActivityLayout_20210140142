47bb76ff3643b261b6436bdccbf79086e988b4bf
DhioAWP <dhioanugerah0@gmail.com>
2023-10-05 20:26:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectpertemuan2/LinaerLayout.kt
@@ -0,0 +1,11 @@
+package com.example.projectpertemuan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinaerLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linaer_layout)
+    }
+}
---
+++ app/src/main/java/com/example/projectpertemuan2/frame_layout.kt
@@ -0,0 +1,11 @@
+package com.example.projectpertemuan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class frame_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
